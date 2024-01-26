52f4e63e69436e8edb1edff8bfa284218348dc76
Toon Van den Bos <toon@whitecube.be>
2024-01-26 16:10:37 +0100

    Fixed Unit tests when running along Feature tests

--- tests/DefaultTestCase.php
+++ tests/DefaultTestCase.php
@@ -6,4 +6,10 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 class DefaultTestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+        // Fixes Unit tests failing when running alon Feature tests:
+        \Illuminate\Support\Facades\Facade::setFacadeApplication(null);
+    }
 }
